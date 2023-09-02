4ea6d1035980233e15b61f2c6f087babb69513aa
Ernisto <joaovitordesouza707@gmail.com>
2023-09-01 21:35:55 -0300

    initial commit

---
+++ src/Connection.lua
@@ -0,0 +1,73 @@
+--!strict
+
+--[=[
+	@class Connection
+	
+	A class that represents a listener of Signals, returned by Signal:Connect() and Signal:Once().
+	Useful to handle if callback will be called or not.
+]=]
+local Connection = {}
+
+--[=[
+    @within Connection
+    @function new
+    @param signal callbacks_container
+    @param callback: (...any) -> ...any
+    @return Connection
+    
+    Create a connection binded to given `signal`
+]=]
+type callbacks_container = {
+    remove: (self: callbacks_container, connection: Connection) -> (),
+    add: (self: callbacks_container, connection: Connection) -> (),
+}
+function Connection.new(signal: callbacks_container, callback: (...any) -> any...)
+	
+	local meta = { __metatable = "locked", __call = callback }
+	local self = setmetatable({}, meta)
+	
+	--[=[
+		@within Connection
+		@prop isConnected boolean
+		
+		Determine if connection is connected or not
+	]=]
+	self.isConnected = true
+	
+	--[=[
+		@within Connection
+		@method disconnect
+		
+		Does the callback stop to be called when signal has fired.
+	]=]
+	function self:disconnect()
+		
+		self.IsConnected = false
+		signal:_remove(self)
+	end
+	
+	--[=[
+		@within Connection
+		@method reconnect
+		
+		Does the callback begin to be called when signal has fired.
+	]=]
+	function self:reconnect()
+		
+		self.IsConnected = true
+		signal:_add(self)
+	end
+	
+	--// Behaviour
+	function meta:__tostring()
+		
+		return `Connection({if self.IsConnected then "connected" else "disconnected"} to '{signal:GetFullName()}')`
+	end
+	
+	--// End
+	return self
+end
+
+--// End
+export type Connection = typeof(Connection.new())
+return Connection
